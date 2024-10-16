2cdcfd633c7735ce6de7171f8f3f68aa55d347ef
Dan DeGenaro <daniel.degenaro@gmail.com>
2024-10-16 10:55:59 -0400

    attempt 4

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import torch
-import torch.nn as nn
+from torch import nn
 import torch.nn.functional as F
 
 class LeNet(nn.Module):
@@ -7,7 +7,7 @@ class LeNet(nn.Module):
     def __init__(self):
         super(LeNet, self).__init__()
         # 1 input image channel, 6 output channels, 5x5 square convolution
-        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv1 = nn.Conv2d(1, 6, 5, padding=2)
         # 6 input channels to 16 output channels with 5x5 convolution
         self.conv2 = nn.Conv2d(6, 16, 5)
         # affine operations: y = Wx + b
@@ -17,7 +17,7 @@ class LeNet(nn.Module):
 
     def forward(self, x):
         # average pooling over a 2x2 window
-        x = F.avg_pool2d(F.sigmoid(self.conv1(x)), 2, padding=1)
+        x = F.avg_pool2d(F.sigmoid(self.conv1(x)), (2, 2))
         # If the size is a square, you can specify with a single number
         x = F.avg_pool2d(F.sigmoid(self.conv2(x)), 2)
         x = torch.flatten(x, 1) # flatten all dimensions except the batch dimension
