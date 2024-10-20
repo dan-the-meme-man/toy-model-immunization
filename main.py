5499ad6b17541079511c976fdf026d877af7e17b
Dan DeGenaro <daniel.degenaro@gmail.com>
2024-10-20 17:23:43 -0400

    added FT

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import os
 import argparse
 from time import time
 import torch
 from torch.nn import CrossEntropyLoss
 import torch.optim as optim
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
 from torchvision import transforms
 from torchvision.datasets import MNIST
 
@@ -42,12 +43,23 @@ mnist_transform = transforms.Compose([
     transforms.Normalize((0.1308,), (0.3016,))
 ])
 
-train_dataset = MNIST(
+full_train_dataset = MNIST(
     root='./data',
     train=True,
     download=True,
     transform=mnist_transform
 )
+
+holdout_classes = (0, 1)
+holdout_indices = [
+    i for i, target in enumerate(full_train_dataset.targets) if target in holdout_classes
+]
+train_indices = [
+    i for i in range(len(full_train_dataset)) if i not in holdout_indices
+]
+
+train_dataset = Subset(full_train_dataset, train_indices)
+ft_dataset = Subset(full_train_dataset, holdout_indices)
 
 val_dataset = MNIST(
     root='./data',
@@ -58,6 +70,12 @@ val_dataset = MNIST(
 
 train_loader = DataLoader(
     train_dataset,
+    batch_size=batch_size,
+    shuffle=True,
+    num_workers=4
+)
+ft_loader = DataLoader(
+    ft_dataset,
     batch_size=batch_size,
     shuffle=True,
     num_workers=4
@@ -116,3 +134,51 @@ print(f'total time: {time() - start:.3f} seconds')
 
 name = f'{args.criterion}_{"_".join([str(x) for x in alpha_schedule])}'
 torch.save(m.state_dict(), f'{name}.pth')
+
+"""FINE-TUNING"""
+optimizer = optim.Adam(m.parameters(), lr=1e-4)
+criterion = CrossEntropyLoss()
+best_err1 = 100
+best_err5 = 100
+
+start = time()
+
+# epoch loop
+for epoch in range(0, epochs):
+
+    # train for one epoch
+    train_loss = train(
+        ft_loader,
+        m,
+        criterion,
+        optimizer,
+        epoch,
+        epochs
+    )
+
+    # evaluate on validation set
+    err1, err5, val_loss = validate(
+        val_loader,
+        m,
+        criterion,
+        epoch,
+        epochs
+    )
+
+    # remember best prec@1 and save checkpoint
+    is_best = err1 <= best_err1
+    best_err1 = min(err1, best_err1)
+    if is_best:
+        best_err5 = err5
+
+    print('FT Current best error rate (top-1 and top-5 error):', best_err1, best_err5, '\n')
+print('FT Best error rate (top-1 and top-5 error):', best_err1, best_err5)
+
+print('\nFT criterion: CrossEntropyLoss')
+print('optimizer:', optimizer, 'lr:', optimizer.param_groups[0]['lr'])
+print('batch_size:', batch_size)
+print('epochs:', epochs)
+print(f'total time: {time() - start:.3f} seconds')
+
+name = f'{args.criterion}_{"_".join([str(x) for x in alpha_schedule])}'
+torch.save(m.state_dict(), f'ft_{name}.pth')
